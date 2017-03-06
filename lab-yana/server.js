e3f1733f4b3e0eb4461beb20b589a4294227e3fa
radenska <yana_radenska@yahoo.com>
2017-03-06 14:34:51 -0800

    added authorization middleware

--- lab-yana/server.js
+++ lab-yana/server.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const express = require('express');
 const Promise = require('bluebird');
-// const authRouter = require('./route/auth-router.js');
+const authRouter = require('./route/auth-router.js');
 const errors = require('./lib/error-middleware.js');
 const PORT = process.env.PORT || 3004;
 
@@ -14,7 +14,7 @@ const app = express();
 
 app.use(cors());
 app.use(morgan('dev'));
-// app.use(authRouter);
+app.use(authRouter);
 app.use(errors);
 
 app.listen(PORT, () => debug(`Listening on port ${PORT}.`));
