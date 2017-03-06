7123ab9f2a3d43b257728eea968e4e82670f660b
radenska <yana_radenska@yahoo.com>
2017-03-06 14:51:31 -0800

    added readme.md text, GET and POST routes, and made small changes to server.js

--- lab-yana/server.js
+++ lab-yana/server.js
@@ -5,12 +5,16 @@ const morgan = require('morgan');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const express = require('express');
-const Promise = require('bluebird');
-const authRouter = require('./route/auth-router.js');
+const authRouter = require('./route/auth-routes.js');
 const errors = require('./lib/error-middleware.js');
+const dotenv = require('dotenv');
 const PORT = process.env.PORT || 3004;
 
+dotenv.load(); //use the variables in the .env file;
+
 const app = express();
+
+mongoose.connect(process.env.MONGODB_URI);
 
 app.use(cors());
 app.use(morgan('dev'));
