eb176838af5625174e29ca916f18ffc4aba2fe8c
radenska <yana_radenska@yahoo.com>
2017-03-06 13:47:39 -0800

    added three variables to .env, added .env to .gitignore, wrote four proudly failing tests, wrote err-r imddleware and included in server.js

--- lab-yana/server.js
+++ lab-yana/server.js
@@ -7,13 +7,14 @@ const mongoose = require('mongoose');
 const express = require('express');
 const Promise = require('bluebird');
 // const authRouter = require('./route/auth-router.js');
-// const errors = require('./lib/error-middleware.js');
-const PORT = 3003;
+const errors = require('./lib/error-middleware.js');
+const PORT = process.env.PORT || 3004;
 
 const app = express();
 
 app.use(cors());
+app.use(morgan('dev'));
 // app.use(authRouter);
-// app.use(errors);
+app.use(errors);
 
 app.listen(PORT, () => debug(`Listening on port ${PORT}.`));
