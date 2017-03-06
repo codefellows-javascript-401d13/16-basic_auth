184b28736141f7e6d4e8ac7b5f2fba979fbf6a55
Dana Kulp <dkulp23@gmail.com>
2017-03-06 13:57:43 -0800

    Followed online tutorial on workaround for .env file. Typed out error middleware again.

--- server.js
+++ server.js
@@ -2,10 +2,15 @@
 
 const express = require('express');
 const debug = require('debug')('cfgram:server');
+const morgan = require('morgan');
+const errors = require('./lib/error-middleware.js');
 
 const PORT = process.env.PORT || 3000;
-
 const app = express();
+
+app.use(morgan('dev'));
+
+app.use(errors);
 
 app.listen(PORT, () => {
   debug(`Server's up on PORT: ${PORT}`);
