dc87d2c9481beb77b83fe9c6c6b9e4aaa7b40ada
Remil Marzan <remilonwheels@gmail.com>
2017-03-07 17:47:43 -0800

    completed Gallery GET route and passed 200 test

--- lab-remil/route/gallery-router.js
+++ lab-remil/route/gallery-router.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const debug = require('debug')('ayogram:gallery-router');
+const createError = require('http-errors');
 const jsonParser = require('body-parser').json();
 
 const bearerAuth = require('../lib/bearer-auth-middleware.js');
@@ -14,5 +15,17 @@ galleryRouter.post('/api/gallery', bearerAuth, jsonParser, function(req, res, ne
   req.body.userID = req.user._id;
   new Gallery(req.body).save()
   .then( gallery => res.json(gallery))
+  .catch(next);
+});
+
+galleryRouter.get('/api/gallery/:id', bearerAuth, function(req, res, next) {
+  debug('GET: /api/gallery/:id');
+
+  Gallery.findById(req.params.id)
+  .then( gallery => {
+    if (gallery.userID.toString() !== req.user._id.toString()) return next(createError(401, 'invalid user'));
+
+    return res.json(gallery);
+  })
   .catch(next);
 });
--- lab-remil/test/gallery-router-test.js
+++ lab-remil/test/gallery-router-test.js
@@ -21,28 +21,50 @@ const exampleGallery = {
 };
 
 describe('Gallery Routes', function() {
+  //////
+  // Setup Hooks for ALL Routes
+  /////
+
+  // Clear User and Gallery before all tests
   before( () => {
     return Promise.all([ User.remove({}), Gallery.remove({}) ])
     .catch(Promise.reject);
   });
 
+  // Create a User and token before all tests
+  beforeEach( () => {
+    return new User(exampleUser)
+    .generatePasswordHash(exampleUser.password)
+    .then( user => user.save())
+    .then( user => {
+      this.tempUser = user;
+      return user.generateToken();
+    })
+    .then( token => {
+      this.tempToken = token;
+      return Promise.resolve();
+    })
+    .catch(Promise.reject);
+  });
+
+  // Clear User and Gallery after all tests
   afterEach( () => {
     return Promise.all([ User.remove({}), Gallery.remove({}) ])
     .catch(Promise.reject);
   });
 
-  describe('POST /api/gallery', function() {
-    beforeEach( () => {
-      return new User(exampleUser)
-      .generatePasswordHash(exampleUser.password)
-      .then( user => user.save())
-      .then( user => {
-        this.tempUser = user;
-        return user.generateToken();
-      })
-      .then( token => this.tempToken = token)
-      .catch(Promise.reject);
-    });
+  describe('POST /api/gallery', () => {
+    // beforeEach( () => {
+    //   return new User(exampleUser)
+    //   .generatePasswordHash(exampleUser.password)
+    //   .then( user => user.save())
+    //   .then( user => {
+    //     this.tempUser = user;
+    //     return user.generateToken();
+    //   })
+    //   .then( token => this.tempToken = token)
+    //   .catch(Promise.reject);
+    // });
 
     describe('with a valid body', () => {
       it('should return a gallery', () => {
@@ -59,6 +81,35 @@ describe('Gallery Routes', function() {
           expect(res.body.desc).to.equal(exampleGallery.desc);
           expect(res.body.userID).to.equal(this.tempUser._id.toString());
           expect(date).to.not.equal('Invalid Date');
+        })
+        .catch(Promise.reject);
+      });
+    });
+  });
+
+  describe('GET /api/gallery/:id', () => {
+    beforeEach( () => {
+      console.log('tempToken:', this.tempToken);
+      console.log('tempUser:', this.tempUser);
+      exampleGallery.userID = this.tempUser._id.toString();
+      return new Gallery(exampleGallery).save()
+      .then( gallery => {
+        this.tempGallery = gallery;
+        return Promise.resolve();
+      })
+      .catch(Promise.reject);
+    });
+
+    afterEach( () => delete exampleGallery.userID);
+
+    describe('with a valid id', () => {
+      it('should return a gallery', () => {
+        return request.get(`${url}/api/gallery/${this.tempGallery._id}`)
+        .set({
+          Authorization: `Bearer ${this.tempToken}`,
+        })
+        .then( res => {
+          expect(res.status).to.equal(200);
         })
         .catch(Promise.reject);
       });
