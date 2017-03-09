18b7cbebd45f89cbe7292ac7a021786b4e0d27ee
brials <brials008@hotmail.com>
2017-03-09 15:09:35 -0800

    I hate aws mocks

--- test/pic-route-test.js
+++ test/pic-route-test.js
@@ -5,7 +5,7 @@ require('./lib/test-env.js');
 const expect = require('chai').expect;
 const request = require('superagent');
 const debug = require('debug')('cfrgam:pic-route-test');
-const awsMocks = require('./lib/aws-mocks.js');
+const awsMocks = require('./lib/aws-mocks.js'); //eslint-disable-line
 const Pic = require('../model/pic.js');
 const User = require('../model/user.js');
 const Gallery = require('../model/gallery.js');
