c61e6878fe75225c4ad47a09b0a6b29b134f6b39
GLSea1979 <builtbygl@gmail.com>
2017-03-08 17:14:48 -0800

    one test not passing

--- route/gallery-router.js
+++ route/gallery-router.js
@@ -5,6 +5,7 @@ const jsonParser = require('body-parser').json();
 const createError = require('http-errors');
 const debug = require('debug')('glgram:gallery-router');
 
+
 const Gallery = require('../model/gallery.js');
 const bearerAuth = require('../lib/bearer-auth-middleware.js');
 
